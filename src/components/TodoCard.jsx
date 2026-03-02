a062bea2a12141feba6df370b814115cf27ebc15
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2026-03-02 04:55:03 +0000

    feat: implement TodoCard component

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,5 +1,21 @@
-export function TodoCard() {
+export function TodoCard(props) {
+    const { todo, handleDeleteTodo, todoIndex, handleCompleteTodo } = props
+
     return (
-        <div></div>
+        <div className="todo-item">
+            <p>{todo.input}</p>
+            <div className="todo-buttons">
+                <button className="button-success" onClick={() => {
+                    handleCompleteTodo(todoIndex)
+                }} disabled={todo.complete}>
+                    <i class="fa-solid fa-check"></i>
+                </button>
+                <button className="button-danger" onClick={() =>{
+                    handleDeleteTodo(todoIndex)
+                }}>
+                    <i class="fa-solid fa-minus"></i>
+                </button>
+            </div>
+        </div>
     )
 }
